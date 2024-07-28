db596b81e1f62dcf94142843ec34c8afe1267809
charmingruby <mrrubycanal3105@gmail.com>
2024-07-28 11:22:59 -0300

    feat: common .env func reader

--- gateway/cmd/gateway/main.go
+++ gateway/cmd/gateway/main.go
@@ -4,15 +4,16 @@ import (
 	"log"
 	"net/http"
 
+	_ "github.com/joho/godotenv/autoload"
+
+	common "github.com/charmingruby/remy-common"
 	"github.com/charmingruby/remy-gateway/internal/transport"
 	"github.com/charmingruby/remy-gateway/internal/transport/rest"
 )
 
-const (
-	httpAddr = ":8000"
-)
-
 func main() {
+	httpAddr := common.EnvString("HTTP_ADDR", ":8080")
+
 	mux := http.NewServeMux()
 	restHandler := rest.NewHandler(mux)
 	restHandler.Register()
